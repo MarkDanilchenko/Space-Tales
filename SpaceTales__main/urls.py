c5409571ea980d05da4ee586c0ff13f1752acfd2
MarkDanilchenko <my.ucheba.2023@gmail.com>
2023-10-28 17:34:33 +0300

    Add Films data

--- SpaceTales__main/urls.py
+++ SpaceTales__main/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
+    # films
+    path("films/", views.films__list, name="films__list"),
+    re_path(r'films/(?P<title>[\w\s]+)/$', views.films__details, name='films__details'),
+    # heroes
+    # planets
+    # spaceships
 ]
--- SpaceTales__main/views.py
+++ SpaceTales__main/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+import requests
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from . import forms
 
 
+# user registration
+# user registration
+# user registration
 def registration(request):
-    pass
+    if request.method == "POST":
+        form = forms.UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data("username")
+            password = form.cleaned_data("password1")
+            userLogIn = authenticate(username=username, password=password)
+            login(request, userLogIn)
+            return redirect("/")
+    else:
+        form = forms.UserForm()
+    return render(request, "registration.html", {"form": form})
+
+
+# root URL
+rootURL = "https://swapi.dev/api/"
+
+
+# films functions
+# films functions
+# films functions
+def films__list(request):
+    url = rootURL + "films/"
+    response = requests.get(url)
+    data = response.json()
+    return render(request, "films__list.html", {"data": data})
+
+
+def films__details(request, title):
+    url = rootURL + "films/" + "?search=" + title
+    response = requests.get(url)
+    data = (response.json()).get("results")[0]
+    return render(request, "films__details.html", {"data": data})
